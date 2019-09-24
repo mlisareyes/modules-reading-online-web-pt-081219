4ce2e3501df9b9f8e3ca010135eb18b6d8c2807c
Monica R <mlisareyes@gmail.com>
2019-09-24 19:33:09 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,17 +1,6 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
-  =begin class Kid
-    include Dance
-    extend MetaDancing
-    
-    attr_accessor :name
-    
-    def initialize(name)
-      @name = name
-    end
-  end =end
-  
 class Kid
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
