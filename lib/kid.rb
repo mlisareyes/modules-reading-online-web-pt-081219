0b4dd016dddadb9d6226fcefa48a5f215b086c7b
Monica R <mlisareyes@gmail.com>
2019-09-24 19:29:59 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,13 +1,13 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
-class Kid
-  include Dance
-  extend MetaDancing
-  
-  attr_accessor :name
-  
-  def initialize(name)
-    @name = name
+  class Kid
+    include Dance
+    extend MetaDancing
+    
+    attr_accessor :name
+    
+    def initialize(name)
+      @name = name
+    end
   end
-end
